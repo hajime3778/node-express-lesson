aa2af8a496dee1f21b70b25ebced31e22fc97f97
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-01-16 01:28:19 +0900

    CRUD作成

--- javascript/index.js
+++ javascript/index.js
@@ -1,22 +1,90 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+const mysql = require('mysql');
 
-var server = app.listen(3000, function(){
+// expressで4000ポートにサーバー起動
+const server = app.listen(4000, function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-var todoList = [
-  {
-      id: 0,
-      title: "Sample1",
-      description: "Sample1"
-  },{
-    id: 1,
-    title: "Sample2",
-    description: "Sample2"
+// expressの設定 (cors method header 許可の設定)
+app.disable('x-powered-by');
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, access_token'
+  );
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  } else {
+    next();
   }
-]
+});
 
-app.get("/", function(req, res, next){
-    res.json(todoList);
+// mysqlに接続
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'user',
+  password: 'password',
+  database: 'sample_database',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('connected');
+});
+
+// todoすべてを取得する
+app.get("/api/todos", (req, res, next) => {
+  const sql = 'select * from todos';
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+    res.json(results);
+  });
+});
+
+// todo1件を取得する
+app.get("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id;
+  const sql = 'select * from todos where ?';
+  connection.query(sql, {id: id}, (err, results) => {
+    if (err) throw err;
+    res.json(results[0]);
+  });
+});
+
+// todo1件を作成する
+app.post("/api/todos", (req, res, next) => {
+  const todo = req.body;
+  const sql = 'insert into todos set ?';
+  connection.query(sql, todo, (err, result) => {
+    if (err) throw err;
+    res.status(201).json(result.id);
+  });
+});
+
+// todo1件を更新する
+app.put("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id;
+  const todo = req.body;
+  const sql = 'update todos set ? where ?';
+  connection.query(sql, [todo, {id: id}], (err, result) => {
+    if (err) throw err;
+    res.status(200).send();
+  });
+});
+
+// todo1件を削除する
+app.delete("/api/todos/:id", (req, res, next) => {
+  const id = req.params.id;
+  const sql = 'delete from todos where ?';
+  connection.query(sql, {id: id}, (err, results) => {
+    if (err) throw err;
+    res.status(204).send();
+  });
 });
