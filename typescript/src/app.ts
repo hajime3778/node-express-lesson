c5746a2cbabddb9f53428c37e568633d4a2264f0
Hajime Takeuchi <shadowthe8156@gmail.com>
2021-01-17 00:40:37 +0900

    typescript作成

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,14 +1,17 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const cors = require('cors');
+import express, { Request, Response, NextFunction } from 'express';
+import mysql from 'mysql';
+import { AddressInfo } from 'net';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+
 const app = express();
-const mysql = require('mysql');
 
 //#region expressでWebサーバーの設定
 
 // expressで4000ポートにサーバー起動
-const server = app.listen(4000, function(){
-  console.log("Node.js is listening to PORT:" + server.address().port);
+const server = app.listen(4000, () => {
+  const address = server.address() as AddressInfo;
+  console.log("Node.js is listening to PORT:" + address.port);
 });
 
 // expressの設定 (cors method header 許可の設定)
@@ -16,13 +19,14 @@ app.disable('x-powered-by');
 app.use(cors()).use(bodyParser.json());
 
 // cors を使用せず手動で設定すると以下のような感じになる
-// app.use((req, res, next) => {
+// app.use((req: Request, res: Response, next:NextFunction ) => {
 //   res.header('Access-Control-Allow-Origin', '*');
 //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 //   res.header(
 //     'Access-Control-Allow-Headers',
 //     'Content-Type, Authorization, access_token'
 //   );
+
 //   if ('OPTIONS' === req.method) {
 //     res.send(200);
 //   } else {
@@ -34,9 +38,8 @@ app.use(cors()).use(bodyParser.json());
 
 //#region mysqlに接続
 
-// mysqlに接続
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '',
   port: 3306,
   user: 'user',
   password: 'password',
@@ -53,7 +56,7 @@ connection.connect((err) => {
 //#region APIのエンドポイント(APIに接続するためのURL)を設定
 
 // todoすべてを取得する
-app.get("/api/todos", (req, res, next) => {
+app.get("/api/todos", (req: Request, res: Response, next: NextFunction) => {
   const sql = 'select * from todos';
   connection.query(sql, (err, results) => {
     if (err) throw err;
@@ -62,7 +65,7 @@ app.get("/api/todos", (req, res, next) => {
 });
 
 // todo1件を取得する
-app.get("/api/todos/:id", (req, res, next) => {
+app.get("/api/todos/:id", (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
   const sql = 'select * from todos where ?';
   connection.query(sql, {id: id}, (err, results) => {
@@ -72,7 +75,7 @@ app.get("/api/todos/:id", (req, res, next) => {
 });
 
 // todo1件を作成する
-app.post("/api/todos", (req, res, next) => {
+app.post("/api/todos", (req: Request, res: Response, next: NextFunction) => {
   const todo = req.body;
   const sql = 'insert into todos set ?';
   connection.query(sql, todo, (err, result) => {
@@ -82,21 +85,21 @@ app.post("/api/todos", (req, res, next) => {
 });
 
 // todo1件を更新する
-app.put("/api/todos/:id", (req, res, next) => {
+app.put("/api/todos/:id", (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
   const todo = req.body;
   const sql = 'update todos set ? where ?';
-  connection.query(sql, [todo, {id: id}], (err, result) => {
+  connection.query(sql, [todo, {id: id}], (err) => {
     if (err) throw err;
     res.status(200).send();
   });
 });
 
 // todo1件を削除する
-app.delete("/api/todos/:id", (req, res, next) => {
+app.delete("/api/todos/:id", (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id;
   const sql = 'delete from todos where ?';
-  connection.query(sql, {id: id}, (err, results) => {
+  connection.query(sql, {id: id}, (err) => {
     if (err) throw err;
     res.status(204).send();
   });
