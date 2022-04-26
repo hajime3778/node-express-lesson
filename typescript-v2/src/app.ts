bdfee7a439f7552c897b2bb02fbb37cb7d991dba
hajime.takeuchi <shadowthe8156@gmail.com>
2022-04-27 00:09:03 +0900

    add db connection sample

--- typescript-v2/src/app.ts
+++ typescript-v2/src/app.ts
@@ -1,29 +1,46 @@
 import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
 import { AddressInfo } from "net";
 import * as dotenv from "dotenv";
+import mysql, { ResultSetHeader, RowDataPacket } from "mysql2/promise";
 
-// envファイル読み込み
-dotenv.config();
-export const { PORT } = process.env;
+async function main() {
+  // envファイル読み込み
+  dotenv.config();
+  const { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASS, MYSQL_DB, PORT, SECRET_KEY } = process.env;
 
-const app: Express = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  const app: Express = express();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
+  const corsOptions = {
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
 
-// expressで4000ポートにサーバー起動
-const server = app.listen(parseInt(PORT as string), () => {
-  const address = server.address() as AddressInfo;
-  console.log("Node.js is listening to PORT:" + address.port);
-});
+  // CORS設定
+  app.use(cors(corsOptions));
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  let foo = "";
-  foo = `{"message": "hello-world"}`;
-  res.json(foo);
-});
+  // expressで4000ポートにサーバー起動
+  const server = app.listen(parseInt(PORT as string), () => {
+    const address = server.address() as AddressInfo;
+    console.log("Node.js is listening to PORT:" + address.port);
+  });
+
+  const connection = await mysql.createConnection({
+    host: MYSQL_HOST as string,
+    port: parseInt(MYSQL_PORT as string),
+    user: MYSQL_USER as string,
+    password: MYSQL_PASS as string,
+    database: MYSQL_DB as string,
+  });
+
+  app.get("/", async (req: express.Request, res: express.Response) => {
+    const sql = "select * from todos";
+    const [rows] = await connection.execute<any[] & RowDataPacket[]>(sql);
+    res.json(rows);
+  });
+}
+
+main();
